3de045c5a5db58dc87a98353d80af3dc7069d963
B <sbslim.dev@gmail.com>
2022-02-09 13:51:54 +0100

    fix step fad

--- src/app/components/Step.tsx
+++ src/app/components/Step.tsx
@@ -9,32 +9,19 @@ interface StepProps {
 
 function Step({ label, index, isLastChd, isSlected }: StepProps) {
   return (
-    <div className="step ">
+    <div className={clsx("step ", !isSlected && "fade")}>
       <div className="step-body">
         <div
-          className={clsx(
-            "step-body__line",
-            index === 0 && "display-none",
-            !isSlected && "fade"
-          )}
+          className={clsx("step-body__line", index === 0 && "display-none")}
         />
-        <div className={clsx("step-body__lvl", !isSlected && "fade")}>
-          {index + 1}
-        </div>
-        <div
-          className={clsx(
-            "step-body__line",
-            isLastChd && "display-none",
-            !isSlected && "fade"
-          )}
-        />
+        <div className="step-body__lvl">{index + 1}</div>
+        <div className={clsx("step-body__line", isLastChd && "display-none")} />
       </div>
       <div
         className={clsx(
           "step-label",
           index === 0 && "step-label--firstChd",
-          isLastChd && "step-label--lastChd",
-          !isSlected && "fade"
+          isLastChd && "step-label--lastChd"
         )}
       >
         {label}
